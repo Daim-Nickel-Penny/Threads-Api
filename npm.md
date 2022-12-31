3885b3c4225df2c2141768bf8a5522eba4ab58f0
Daim-Nickel-Penny <1804449@kiit.ac.in>
2022-12-31 19:54:52 +0530

    Feed Query Done

---
+++ npm.md
@@ -0,0 +1,4 @@
+npx ts-node --transpile-only src/schema
+
+- You can run `npm run generate` to update your schema.graphql and nexus-typegen.ts file when there are any changes in your Nexus code.
+- You can use `npm run dev` to start the web server and watch for any changes.
---
+++ src/graphql/Link.ts
@@ -0,0 +1,36 @@
+import { objectType, extendType } from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.nonNull.int("id");
+    t.nonNull.string("description");
+    t.nonNull.string("url");
+  },
+});
+
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: 1,
+    url: "www.fshk.com",
+    description: "dsgjkfh hsdjfhk",
+  },
+  {
+    id: 2,
+    url: "graphql.org",
+    description: "GraphQL official website",
+  },
+];
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve(parent, args, context, info) {
+        return links;
+      },
+    });
+  },
+});
